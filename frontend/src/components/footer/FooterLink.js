4cf11774cbff8dbbb6dfcf982124fa6c4b902f23
ademibowale <ademibowaleadegboye@gmail.com>
2024-05-17 12:27:55 +0100

    Implement  category

---
+++ frontend/src/components/footer/FooterLink.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './FooterLinks.scss'
+
+const FooterLink = () => {
+  return (
+    <>
+    <section className="contact-section">
+
+    </section>
+    </>
+  )
+}
+
+export default FooterLink
